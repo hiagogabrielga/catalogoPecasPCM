5bda6088d6cebada1aba88c3865e1f9c45192b4b
hiagogabrielga <hiagogabriel1132@gmail.com>
2026-06-17 07:31:33 -0400

    atualização do estoque

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ let atualizacaoDisponivel = false;
 const IMAGEM_PADRAO = "placeholder.png";
 const CACHE_KEY = "catalogoPecasPCM";
 const CACHE_VERSION_KEY = "catalogoPecasPcmVersao";
-const VERSAO_ATUAL = "2.2.17";
+const VERSAO_ATUAL = "2.2.18";
 
 // ===============================
 // UTIL
